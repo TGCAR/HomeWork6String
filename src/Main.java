b9398546aeb4869ce78b2a913b1b7423118cdb96
TGCAR <don.meln2013@yandex.ru>
2024-12-05 10:03:06 +0300

    HomeWork6String

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
+        System.out.println();
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
+        System.out.println("Task 2");
+        System.out.println();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName.toUpperCase());
+        firstName = firstName.replace("Ivan", "Семен");
+        middleName = middleName.replace("Ivanovich", "Семенович");
+        lastName = lastName.replace("Ivanov", "Иванов");
+        fullName =
 
+        System.out.println("Данные Ф.И.О. сотрудника " + fullName);
 
     }
+
+    String firstName = "klm";
+
 }
